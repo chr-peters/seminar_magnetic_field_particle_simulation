075732d610909a166edd55f9c20c796a4dc8c034
cxan96 <c.p.xanten@gmail.com>
2017-09-21 14:14:35 +0200

    initial commit

---
+++ Software/include/foo.h
@@ -0,0 +1,9 @@
+#if !defined(FOO_H)
+#define FOO_H
+
+class Foo {
+public:
+  void bar ();
+};
+
+#endif
---
+++ Software/src/foo.cpp
@@ -0,0 +1,9 @@
+#include "foo.h"
+
+#include<iostream>
+
+using namespace std;
+
+void Foo::bar() {
+  cout << "Bar!" << endl;
+}
