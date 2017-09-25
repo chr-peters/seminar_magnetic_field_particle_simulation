706ecaefe4b1ee06722c38d99d45b0cb9af3ecd9
cxan96 <c.p.xanten@gmail.com>
2017-09-25 15:56:08 +0200

    edited test example so that it opens a window

--- Software/examples/testplot.cpp
+++ Software/examples/testplot.cpp
@@ -1,38 +1,61 @@
-// Builds a graph with errors, displays it and saves it as
-// image. First, include some header files
-// (not necessary for Cling)
-
-#include "TCanvas.h"
-#include "TROOT.h"
-#include "TGraphErrors.h"
 #include "TF1.h"
-#include "TLegend.h"
-#include "TArrow.h"
-#include "TLatex.h"
+#include "TApplication.h"
 #include "TMath.h"
-#include "TGraph.h"
 #include "TGraphPolar.h"
+#include "TCanvas.h"
+#include "TGraph2D.h"
+#include "TAxis.h"
+#include "TF2.h"
+#include "TRandom3.h"
+#include "TGraph2DErrors.h"
+#include "TH1.h"
+#include "TStyle.h"
+#include "TColor.h"
 
-void macro(){
-    auto c = new TCanvas("myCanvas","myCanvas",600,600);
-    Double_t rmin=0.;
-    Double_t rmax=TMath::Pi()*6.;
-    const Int_t npoints=1000;
-    Double_t r[npoints];
-    Double_t theta[npoints];
-    for (Int_t ipt = 0; ipt < npoints; ipt++) {
-        r[ipt] = ipt*(rmax-rmin)/npoints+rmin;
-        theta[ipt] = TMath::Sin(r[ipt]);
-    }
-    TGraphPolar grP1 (npoints,r,theta);
-    grP1.SetTitle("A Fan");
-    grP1.SetLineWidth(3);
-    grP1.SetLineColor(2);
-    grP1.DrawClone("L");
+void plot(){
+  gStyle->SetPalette(kBird);
+  const double e = 0.3;
+  const int nd = 500;
 
-    grP1.Print();
+  TRandom3 my_random_generator;
+  TF2 f2("f2",
+	 "1000*(([0]*sin(x)/x)*([1]*sin(y)/y))+200",
+	 -6,6,-6,6);
+  f2.SetParameters(1,1);
+  TGraph2DErrors dte(nd);
+  // Fill the 2D graph
+  double rnd, x, y, z, ex, ey, ez;
+  for (Int_t i=0; i<nd; i++) {
+    f2.GetRandom2(x,y);
+    // A random number in [-e,e]
+    rnd = my_random_generator.Uniform(-e,e);
+    z = f2.Eval(x,y)*(1+rnd);
+    dte.SetPoint(i,x,y,z);
+    ex = 0.05*my_random_generator.Uniform();
+    ey = 0.05*my_random_generator.Uniform();
+    ez = fabs(z*rnd);
+    dte.SetPointError(i,ex,ey,ez);
+  }
+  // Fit function to generated data
+  f2.SetParameters(0.7,1.5);  // set initial values for fit
+  f2.SetTitle("Fitted 2D function");
+  dte.Fit(&f2);
+  // Plot the result
+  auto c1 = new TCanvas();
+  f2.SetLineWidth(1);
+  f2.SetLineColor(kBlue-5);
+  TF2   *f2c = (TF2*)f2.DrawClone("Surf1");
+  TAxis *Xaxis = f2c->GetXaxis();
+  TAxis *Yaxis = f2c->GetYaxis();
+  TAxis *Zaxis = f2c->GetZaxis();
+  Xaxis->SetTitle("X Title"); Xaxis->SetTitleOffset(1.5);
+  Yaxis->SetTitle("Y Title"); Yaxis->SetTitleOffset(1.5);
+  Zaxis->SetTitle("Z Title"); Zaxis->SetTitleOffset(1.5);
+  dte.DrawClone("P0 Same");
 }
 
-int main(){
-    macro();
+int main(int argc, char**argv){
+  TApplication app("ROOT Application", &argc, argv);
+  plot();
+  app.Run();
 }
